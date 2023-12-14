e97b7cdc809fe65ab1b87b822116f2a4e5204f7d
TomerAsulin <tomermailg@gmail.com>
2023-12-14 20:15:53 +0200

    Create flight list component

--- flight-management-angular/src/app/app.module.ts
+++ flight-management-angular/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlightListComponent } from './flight-list/flight-list.component';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FlightListComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, MatTableModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ flight-management-angular/src/app/flight-list/flight-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FlightService } from '../flight.service';
+
+@Component({
+  selector: 'app-flight-list',
+  templateUrl: './flight-list.component.html',
+  styleUrl: './flight-list.component.scss',
+})
+export class FlightListComponent implements OnInit {
+  constructor(private flightService: FlightService) {}
+
+  flights = this.flightService.getFlights();
+
+  headers: string[] = [
+    'flightNumber',
+    'landingAirport',
+    'takeoffAirport',
+    'status',
+    'takeoffTime',
+    'landingTime',
+  ];
+
+  ngOnInit(): void {}
+}
